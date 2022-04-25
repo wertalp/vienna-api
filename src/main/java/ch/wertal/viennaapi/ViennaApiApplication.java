bcb6f2eea413e85f988ef1922e061cc314207d38
Patrick Wertal <patrick.wertal@gmail.com>
2022-04-25 16:20:27 +0200

    initial not workig h2 db yml

---
+++ src/main/java/ch/wertal/viennaapi/ViennaApiApplication.java
@@ -0,0 +1,13 @@
+package ch.wertal.viennaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViennaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViennaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/wertal/viennaapi/ViennaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wertal.viennaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViennaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
